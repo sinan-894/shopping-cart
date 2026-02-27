2097ae9765b0389bf1a20da5db4421aba17d9453
sinan-894 <mohammedsinan1011@gmail.com>
2026-02-27 10:15:23 +0400

    Write Navbar structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="navbar-pages">
+                <button>HomePage</button>
+                <button>ShopPage</button>
+            </div>
+            <div className="cart-container">
+                <button>cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
