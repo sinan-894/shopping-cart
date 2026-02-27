9c8d869577fdf31554bbf1bdda1e617210329bd3
sinan-894 <mohammedsinan1011@gmail.com>
2026-02-27 11:22:22 +0400

    Create Homepage and style the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar"
 import "./App.css"
+import HomePage from "./components/HomePage"
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <HomePage></HomePage>
     </>
   )
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+
+
+
+function HomePage(){
+    return(
+        <div className="home-page-container">
+            <h1>Welcome to the Website Name !!!</h1>
+            <button>Shop Page</button>
+        </div>
+    )
+}
+
+
+export default HomePage
