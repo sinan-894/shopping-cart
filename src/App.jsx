12eb21e604823fa8fa15c7c3779e6daa1fef6f56
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-01 10:33:41 +0400

    Remove test cart data

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,6 @@ import ShopPage from "./components/ShopPage"
 import CartPage from "./components/CartPage"
 
 
-const cartData = [
-  {
-    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
-    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price:109.95,
-    quantity:5,
-  },
-  {
-    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
-    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price:67.95,
-    quantity:2,
-  },
-  {
-    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
-    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price:9.95,
-    quantity:1,
-  }
-]
 
 
 
@@ -33,7 +13,6 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <CartPage cartData={cartData}></CartPage>
     </>
   )
 }
