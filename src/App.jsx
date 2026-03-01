522a2e5f2ae9389417d253b55e70ab6219a2e2e2
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-01 10:32:27 +0400

    Populate and Test the cart page with valid data

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,35 @@ import ShopPage from "./components/ShopPage"
 import CartPage from "./components/CartPage"
 
 
+const cartData = [
+  {
+    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price:109.95,
+    quantity:5,
+  },
+  {
+    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price:67.95,
+    quantity:2,
+  },
+  {
+    imageurl:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price:9.95,
+    quantity:1,
+  }
+]
+
+
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
-      <CartPage cartData={[1,2,2,3,4,4]}></CartPage>
+      <CartPage cartData={cartData}></CartPage>
     </>
   )
 }
--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -5,14 +5,14 @@ import { Plus,Minus,Trash } from 'lucide-react';
 function CartPage({cartData}){
     // cartData is a array of object of data {title,imageUrl,price,quantity}
 
-    const findTotal = ()=>578347
+    const findTotal = ()=>cartData.reduce((total,item)=>total+(item.price*item.quantity),0)
 
-    const total = findTotal()
+    const total = findTotal().toFixed(2)
     return(
         <div className="cart-list-container">
-            <h1 style={{textAlign:'center'}}>Total : $5435345</h1>
+            <h1 style={{textAlign:'center'}}>Total : ${total}</h1>
             {cartData.map((item)=>(
-                <ListItem></ListItem>
+                <ListItem imageurl={item.imageurl} title={item.title} price={item.price} quantity={item.quantity}></ListItem>
             ))}
 
         </div>
@@ -23,21 +23,21 @@ function CartPage({cartData}){
 export default CartPage
 
 
-function ListItem(){
+function ListItem({imageurl,title,price,quantity}){
     return(
         <div className="list-container">
-            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png" alt="" />
-            <span className="item-title">title</span>
+            <img src={imageurl} alt="" />
+            <span className="item-title">{title}</span>
             <div className="item-qty-container">
                 <button >
                     <Plus size={18}></Plus>
                 </button>
-                <span className="item-quantity">4</span>
+                <span className="item-quantity">{quantity}</span>
                 <button >
                     <Minus size={18}></Minus>
                 </button>
             </div>
-            <span className="item-price">$898080</span>
+            <span className="item-price">${price}</span>
             <button><Trash></Trash></button>
         </div>
     )
