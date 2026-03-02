b0d5602816f7187744a736125a80cb018ec0de60
sinan-894 <mohammedsinan1011@gmail.com>
2026-03-02 16:12:19 +0400

    Make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
 
-import { ShoppingCart } from 'lucide-react';
+import { ShoppingCart,Menu } from 'lucide-react';
+import { useState,useEffect } from 'react';
 import { Link } from "react-router";
 
 
 function Navbar({activePage}){
+    const [width,setWidth] = useState(window.innerWidth)
+    console.log(width)
+    useEffect(()=>{
+        const onResize = ()=>{
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize',onResize)
+        return ()=>{
+            window.removeEventListener('resize',onResize)
+        }
+    },[])
     return(
         <div className="navbar">
-            <div className="navbar-pages">
-                <Link to='/'><button style={{color:(!activePage?'#dfe31d':'#ffffff')}} >Home Page</button></Link>
-                <Link to='/shop'><button style={{color:(activePage=='shop'?'#dfe31d':'#ffffff')}}>Shop Page</button></Link>
-            </div>
+            {(width<750)?(
+                <DropDown></DropDown>
+            ):(
+                <DisplayPageOption activePage={activePage}></DisplayPageOption>
+            )}
             <div className="cart-container">
                 <Link to='/cart'>
                     <button style={{color:(activePage=='cart'?'#dfe31d':'#ffffff')}}> 
@@ -22,3 +35,31 @@ function Navbar({activePage}){
 }
 
 export default Navbar
+
+
+function DisplayPageOption({activePage}){
+    return(
+        <div className="navbar-pages">
+            <Link to='/'><button style={{color:(!activePage?'#dfe31d':'#ffffff')}} >Home Page</button></Link>
+            <Link to='/shop'><button style={{color:(activePage=='shop'?'#dfe31d':'#ffffff')}}>Shop Page</button></Link>
+        </div>
+    )
+}
+
+function DropDown(){
+
+    const [drop,setDrop] = useState(false)
+    return(
+        <div className='drop-down-container'>
+            <button 
+            onClick={()=>setDrop(!drop)}
+            style={{color:(drop)?'#dfe31d':'#ffffff'}}
+            ><Menu></Menu></button>
+            <div className='drop-down' style={{display:(drop)?'block':'none'}}>
+                <Link to='/'><button onClick={()=>setDrop(false)} >Home Page</button></Link>
+                <Link to='/shop'><button onClick={()=>setDrop(false)}>Shop Page</button></Link>
+            </div>
+        </div>
+    )
+    
+}
